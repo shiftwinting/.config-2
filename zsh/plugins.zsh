6af95e63be9d61d17d3db1d9920ba330623d99a9
amei <cms1984@msn.com>
2021-11-15 14:00:59 +0800

    zsh

---
+++ zsh/plugins.zsh
@@ -0,0 +1,7 @@
+export PLUG_DIR=$HOME/.zim
+if [[ ! -d $PLUG_DIR ]]; then
+	#curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+	curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/zimfw/install/master/install.zsh
+	rm ~/.zimrc
+	ln -s ~/dotfiles/zsh/zimrc ~/.zimrc
+fi
